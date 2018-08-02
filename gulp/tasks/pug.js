e8f9360d03e20074aca57276442e34506de9e7e5
Ion Talmaci <ntalmaci171@gmail.com>
2018-08-02 14:40:52 +0300

    adding img for menu

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,7 +4,6 @@ module.exports = function() {
     $.gulp.task('pug', function(){
         return $.gulp.src($.config.src + '/template/pages/*.pug')
         .pipe($.gp.pug({
-            locals : JSON.parse($.fs.readFileSync('./content.json', 'utf8')),
             pretty : true,
         }))
         .on('error', $.gp.notify.onError(function(error){
