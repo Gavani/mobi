c3a68977d693f202f1d0c8c09fa46652f910f05d
Ion Talmaci <ntalmaci171@gmail.com>
2018-08-09 21:08:04 +0300

    adding hamburger menu

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,5 +2,7 @@
 
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
-  './node_modules/@fortawesome/fontawesome-free/js/all.min.js'
+  './node_modules/@fortawesome/fontawesome-free/js/all.min.js',
+  './node_modules/waypoints/lib/jquery.waypoints.min.js',
+  './node_modules/jquery.counterup/jquery.counterup.min.js'
 ];
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,42 @@
-(function(){
-    
-})()
+(function($){
+  $(document).ready(function(){
+    //___________________/Counter/___________________//
+    (function(){
+      $('.counter').counterUp();
+    })();
+
+    //___________________/Hamburger/___________________//
+    (function(){
+      var $checkbox = $('#hamburger__menu-input');
+      var menuHeight = $('.hamburger__menu-wrap').height();
+      var headerheight = $('header').height();
+      var bodyWidth    = $('body').width();
+
+      $('.hamburger__menu-wrap').css({'width': bodyWidth - 150, 'left' :  -bodyWidth});
+        $checkbox.change(function(){
+          var checked = $(this).prop('checked');
+          console.log(checked);
+
+          if(checked == true){
+            $('.wrapper').css('height', menuHeight + headerheight);
+            $('main').addClass('main-covered');
+
+            $(document).mouseup(function (e){ 
+              var div = $(".hamburger__menu"); 
+              if (!div.is(e.target)
+                  && div.has(e.target).length === 0) {
+                  $('.wrapper').css({'min-height':'100%', 'height':'auto'});
+                  $('main').removeClass('main-covered');
+                  $("#hamburger__menu-input").prop("checked", false);
+              }
+            });
+          }
+          else{
+            $('.wrapper').css({'min-height':'100%', 'height':'auto'});
+            $('main').removeClass('main-covered');
+          }
+        })
+    })();
+  })
+
+})(jQuery)
